6b19455cd3786c1d45f668096af6dd915c82ab14
kumak5 <my0yashik@gmail.com>
2021-05-08 02:52:56 +0400

    lesson 14 css modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
-    return <header className='header'>
-        <img src='logo_react.jpg' />
+    return <header className={s.header}>
+        <img src='logo_react.jpg'/>
     </header>
 }
 export default Header;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
+import s from'./Navbar.module.css';
+console.log(s)
+
+// let s = {
+//     'nav': 'Navbar_nav__3SD7X',
+//     'item': 'Navbar_item__nTXSX'
+// }
 
 const Navbar = () => {
-    return <nav className='nav'>
-        <div>
+    return <nav className='Navbar_nav__3SD7X'>
+        <div className='Navbar_item__nTXSX'>
             <a>Profile</a>
         </div>
-        <div>
+        <div className='Navbar_item__nTXSX'>
             <a>Messages</a>
-        </div><div>
-        <a>News</a>
-    </div>
-        <div>
+        </div>
+        <div className={s.item}>
+            <a>News</a>
+        </div>
+        <div className={`${s.item} ${s.active}`}>
             <a>Music</a>
         </div>
-        <div>
+        <div className='item'>
             <a>Settings</a>
         </div>
     </nav>

2d288e19f4a468ea56704f3ad1c93e5de2509677
kumak5 <my0yashik@gmail.com>
2021-05-11 00:46:30 +0400

    lesson 25 massive

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
-const DialogItem = (props) =>{
-    let path = '/dialogs/'+props.id
+const DialogItem = (props) => {
+    let path = '/dialogs/' + props.id
 
     return <div className={s.dialogs + ' ' + s.active}>
         <NavLink to={path}>{props.name}</NavLink>
@@ -14,20 +14,41 @@ const Message = (props) => {
     return <div className={s.dialog}>{props.message}</div>
 }
 
+let dialogs = [
+    {id: 1, name: 'Kolya'},
+    {id: 2, name: 'Vasya'},
+    {id: 3, name: 'Petya'},
+    {id: 4, name: 'Nataliya'},
+    {id: 4, name: 'Nikolya'},
+    {id: 5, name: 'Anastasiya'}
+]
+
+let messages = [
+    {id: 1, message: 'Hi1'},
+    {id: 2, message: 'Hi2'},
+    {id: 3, message: 'Hi3'},
+    {id: 4, message: 'Hi4'},
+    {id: 5, message: 'Hi5'},
+    {id: 6, message: 'Hi6'}
+]
+
+let dialogsElemenst = dialogs.map(d=><DialogItem name={d.name} id={d.id}/>)
+
+let messageElements = messages.map( m =><Message message={m.message}/>)
+
 const Dialogs = (props) => {
     return <div className={s.dialogs}>
         <div className={s.dialogsItems}>
-            <DialogItem name='Kolya' id='1'/>
-            <DialogItem name='Vasya' id='2'/>
-            <DialogItem name='Petya' id='3'/>
-            <DialogItem name='Nataliya' id='4'/>
-            <DialogItem name='Anastasiya' id='5'/>
+            {dialogsElemenst}
+            {/*<DialogItem name={dialogs[0].name} id={dialogs[0].id}/>*/}
+            {/*<DialogItem name={dialogs[1].name} id={dialogs[1].id}/>*/}
         </div>
-        <div className={s.messages}>
-            <Message message='Hi1'/>
-            <Message message='Hi2'/>
-            <Message message='Hi3'/>
-        </div>
+
+    <div className={s.messages}>
+        {messageElements}
+        {/*<Message message={messages[0].message}/>*/}
+        {/*<Message message={messages[1].message}/>*/}
     </div>
+</div>
 }
 export default Dialogs;
--- src/components/Profile/MyPost/MyPost.jsx
+++ src/components/Profile/MyPost/MyPost.jsx
@@ -3,6 +3,14 @@ import s from './MyPost.module.css';
 import Post from "./Post/Post";
 
 const MyPost = () => {
+
+    let posts = [
+        {id: 0, message:'Hi, how are you?', likesCount: 0},
+        {id: 1, message:'It\'s my first post', likesCount: 25}
+    ]
+
+    let postsElements = posts.map(p=><Post message={p.message} likesCount={p.likesCount}/>)
+
     return <div className={s.postBlock}>
 
         <h3>My post</h3>
@@ -15,8 +23,7 @@ const MyPost = () => {
             </div>
         </div>
         <div className={s.posts}>
-            <Post message='Hi, how are you?' likesCount='0'/>
-            <Post message='It`s my first post' likesCount='25'/>
+            {postsElements}
 
         </div>
 

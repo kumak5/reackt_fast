3491734631621e2d2485beff422902eb06ce877b
kumak5 <my0yashik@gmail.com>
2021-06-07 19:02:42 +0400

    lesson 54 componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,13 +5,12 @@ import * as axios from 'axios';
 
 class Users extends React.Component {
 
-    constructor(props) {
-        super(props);
-        alert('new')
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(response => {
                 this.props.setUsers(response.data.items);
             });
+
     }
 
     render() {
